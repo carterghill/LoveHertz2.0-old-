a6aaf48ec8c214a40188ae096277216a0d9d6cc1
Carter Hill <carterghill@gmail.com>
2016-08-22 17:14:47 -0600

    Fixed collision glitch

--- collision.lua
+++ collision.lua
@@ -5,16 +5,16 @@ function collision( player, dt )
 	while count <= table.getn(objects) do
 		if player.ySpeed > 0 and count ~= player.id and player.y < objects[count].y then
 			if player.y + player.height + player.ySpeed*dt > objects[count].y then
-				if player.x < objects[count].x + objects[count].width + 3
-				and player.x + player.width > objects[count].x then
+				if player.x < objects[count].x + objects[count].width - 2
+				and player.x + player.width > objects[count].x +2 then
 					player.ySpeed = 0
 					player.y = objects[count].y - player.height
 				end
 			end			
 		elseif player.ySpeed < 0 and count ~= player.id and player.y > objects[count].y then
 			if player.y - player.ySpeed*dt - 3  < objects[count].y + objects[count].height then
-				if player.x < objects[count].x + objects[count].width +4
-				and player.x + player.width > objects[count].x then
+				if player.x < objects[count].x + objects[count].width -2
+				and player.x + player.width -2 > objects[count].x then
 					player.ySpeed = 0
 					player.y = objects[count].y + objects[count].height
 				end
@@ -24,7 +24,7 @@ function collision( player, dt )
 		if player.xSpeed > 0 and count ~= player.id and player.x < objects[count].x then
 			if player.x + player.width + player.xSpeed*dt > objects[count].x then
 				if player.y < objects[count].y + objects[count].height
-				and player.y + player.height > objects[count].y then
+				and player.y + player.height - 2 > objects[count].y then
 					player.xSpeed = 0
 					player.x = objects[count].x - player.width
 				end
@@ -32,9 +32,9 @@ function collision( player, dt )
 		elseif player.xSpeed < 0 and count ~= player.id and player.x > objects[count].x then
 			if player.x - player.xSpeed*dt - 3  < objects[count].x + objects[count].height +3 then
 				if player.y < objects[count].y + objects[count].width
-				and player.y + player.width + 3> objects[count].y then
+				and player.y + player.height > objects[count].y then
 					player.xSpeed = 0
-					player.x = objects[count].x + objects[count].height
+					player.x = objects[count].x + objects[count].width
 				end
 			end			
 		end
